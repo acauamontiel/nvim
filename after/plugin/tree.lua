072cdd3fba2d20de428a06970089ed99bc8223f4
Acauã Montiel <contato@acauamontiel.com.br>
2024-03-11 19:54:30 -0300

    fix .gitignore

---
+++ after/plugin/tree.lua
@@ -0,0 +1,19 @@
+require('nvim-tree').setup({
+  renderer = {
+    icons = {
+      glyphs = {
+        folder = {
+          arrow_closed = '+',
+          arrow_open = '-'
+        }
+      }
+    }
+  },
+  filters = {
+    dotfiles = false,
+    custom = { '^.git$' }
+  }
+})
+
+vim.keymap.set('n', '<leader>tt', vim.cmd.NvimTreeToggle)
+vim.keymap.set('n', '<leader>tf', vim.cmd.NvimTreeFindFile)
